b20923c865c3708749319098a2b594f554312cac
Teresa <teriv1707@gmail.com>
2023-01-26 14:18:16 +0100

    ajouter spring boot security

--- src/main/java/fr/doranco/swagger/controller/EmployeeController.java
+++ src/main/java/fr/doranco/swagger/controller/EmployeeController.java
@@ -3,30 +3,25 @@ package fr.doranco.swagger.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import fr.doranco.swagger.model.Employee;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @RestController
+@SecurityRequirement(name = "swaggerapi")
 public class EmployeeController {
 	private List<Employee> employees = createList();
 
-	@RequestMapping(value = "/employees", method = RequestMethod.GET, produces = "application/json")
+	@GetMapping(value = "/employees", produces = "application/json")
 	public List<Employee> firstPage() {
 		return employees;
 	}
 
 	@DeleteMapping(path = { "/{id}" })
-	public Employee delete(@PathVariable("id") int id) {
-		Employee deletedEmp = null;
+	public Employee delete(@PathVariable("id") String id) {
+		Employee deletedEmp=null;
 		for (Employee emp : employees) {
-			if (emp.getEmpId().equals(id)) {
+			if (emp.getEmpId().equals(id) ){
 				employees.remove(emp);
 				deletedEmp = emp;
 				break;
